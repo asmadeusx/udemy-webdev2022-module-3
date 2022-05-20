f312f40334f48254bbf4b4b06a18c431a2aa744f
asmadeusx <Virus1231@yandex.ru>
2022-05-20 17:06:44 +0300

    59
    59

--- src/js/script.js
+++ src/js/script.js
@@ -33,5 +33,29 @@ $(document).ready(function(){
 
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__back');
+
+    // modal
+
+    $('[data-modal=consultation]').on('click', function() {
+        $('.overlay, #consultation').fadeIn();
+    });
+    
+    $('.button_mini').each(function(i) {
+        $(this).on('click', function() {
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+        });
+        $('.overlay, #order').fadeIn();
+    });
+
+    $('.button_submit').on('click', function() {
+        $('#consultation, #order').fadeOut();
+        $('#thanks').fadeIn();
+    });
+
+    $('.modal__close').on('click', function() {
+        $('.overlay, #consultation, #order, #thanks').fadeOut();
+    });
+    
 });
 
+ 
