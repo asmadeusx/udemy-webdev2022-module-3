77280c79a19e6e46dd11c81dd7609be1a80a3f7d
asmadeusx <Virus1231@yandex.ru>
2022-07-20 15:49:42 +0300

    65
    65

--- dist/js/script.js
+++ dist/js/script.js
@@ -85,11 +85,6 @@ $(document).ready(function(){
 
     $('form').submit(function(e) {
         e.preventDefault();
-
-        if (!$(this).valid()) {
-            return;        
-        }
-
         $.ajax({
             type: "POST",
             url: "mailer/smart.php",
